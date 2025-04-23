6b73c506545778c5b5ec6e20028d2ea88c0ddcba
RafaelaMichelin <rafaelamichelin@gmail.com>
2025-04-22 21:26:09 -0300

    Criando função Criar Lista e arrumando layout dos botões

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 
-//Criando array
-var dadosLista=[];
 
-//função salvar dados
+
+//Criando array
+var dadosLista = [];
+
+
 function salvardados(){
     let categoria = document.getElementById('categoria').value;
     let valor = document.getElementById('valor').value;
     let descricao = document.getElementById('descricao').value;
 
-    if(categoria || valor || descricao){
-       //metodo para colocar o valor dentro do array 
-       dadosLista.push(categoria); 
-       dadosLista.push(valor);
-       dadosLista.push(descricao);
-
+    
+    if(categoria && valor && descricao){
+       
+        dadosLista.push(categoria);
+        dadosLista.push(valor);
+        dadosLista.push(descricao);
        console.log(dadosLista);
-       criaLista();
+       criarLista();
        //apagar o nome do campo input
        document.getElementById("categoria").value = "";
        document.getElementById("valor").value = "";
        document.getElementById("descricao").value = "";
     }
-    else {
-        alert('Favor informar os campos!');
+}
+function criarLista(){
+
+    let tabela = "<tr><td>Categoria</td><td>Valor</td><td>Descricao</td></tr>";
+    for(let i = 0; i<= (dadosLista.length-1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "<td><button class='btn'editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>"; 
+        document.getElementById('tabela').innerHTML = tabela;          
     }
 }
+
+
